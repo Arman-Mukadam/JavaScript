d25fa89f4ecde5e4981e0bd2ee097108596fe7aa
ArmanMukadam <arman@tribox.me>
2024-06-11 02:28:09 +0530

    completed dom manipulation

---
+++ 06_dom/one.js
@@ -0,0 +1,8 @@
+// querySelectorAll returns an NodeList which contains forEach()----returns list
+// getElementsByClassName returns an HtmlCollection which does not contains any looping function----returns list
+// U can convert to array using --- Array.from(tag)
+// innerText shows only visible text not the hidden
+// textContent shows all visible text hidden also
+// innerHTML shows all text and child tags and text also
+// document.querySelector('') here u can grab using all css selectors----return element
+// document.getElementsById -----returns element
